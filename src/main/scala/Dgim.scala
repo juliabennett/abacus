4d8c995e23925f4fb52bfb6e9a36b4e7f0ac13bf
julia <juliacbennett@gmail.com>
2017-11-09 20:42:04 -0800

    Added query method

--- src/main/scala/Dgim.scala
+++ src/main/scala/Dgim.scala
@@ -1,6 +1,5 @@
 
 /* TODO
-- Implement query method
 - Use mod N timestamps
 - Extend to improved error bound
  */
@@ -22,9 +21,18 @@ case class Dgim(windowLength: Long, currentPosition: Long, buckets: Vector[Bucke
     )
   }
 
-  override def toString = {
+  def query(k: Long): Long = scanAndCount(k, buckets)
+
+  override def toString: String = {
     val bucketStr = buckets.mkString("[", ",", "]")
     s"windowLength=$windowLength,currentPosition=$currentPosition,buckets=$bucketStr"
+  }
+
+  private def trimBuckets(): Vector[Bucket] = {
+    buckets match {
+      case start :+ last if last.position + windowLength <= currentPosition + 1 => start
+      case _ => buckets
+    }
   }
 
   @scala.annotation.tailrec
@@ -37,11 +45,24 @@ case class Dgim(windowLength: Long, currentPosition: Long, buckets: Vector[Bucke
     }
   }
 
-  private def trimBuckets(): Vector[Bucket] = {
-    buckets match {
-      case start :+ last if last.position + windowLength <= currentPosition + 1 => start
-      case _ => buckets
+  @scala.annotation.tailrec
+  private def scanAndCount(k: Long, currentBuckets: Vector[Bucket], acc: Long = 0): Long = {
+    currentBuckets match {
+      case first +: second +: tail if currentPosition - second.position < k =>
+        scanAndCount(k, second +: tail, acc + pow2(first.sizeExp))
+      case first +: second +: tail if currentPosition - first.position < k =>
+        acc + pow2(first.sizeExp - 1)
+      case first +: tail if currentPosition - first.position < k =>
+        acc + pow2(first.sizeExp - 1)
+      case _ => acc
     }
+  }
+
+  @scala.annotation.tailrec
+  private def pow2(exp: Long, acc: Long = 1): Long = {
+    if (exp < 0) 0
+    else if (exp == 0) acc
+    else pow2(exp-1, acc*2)
   }
 
 }
